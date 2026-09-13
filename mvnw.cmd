86175bcaf9b19d0b98633c9b8655a80e143ebf2e
Yash Verma <yashverma70070@gmail.com>
2026-09-13 16:01:39 +0530

    Improve mobile UI and prepare app for deployment

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,3 +1,4 @@
+```bat
 @REM ----------------------------------------------------------------------------
 @REM Apache Maven Wrapper startup script (Windows)
 @REM
@@ -19,7 +20,7 @@ set ERROR_CODE=0
 
 @REM Decide JVM
 if not "%JAVA_HOME%"=="" goto OkJHome
-set MAVEN_JAVA_EXE=java.exe
+set "MAVEN_JAVA_EXE=java.exe"
 goto chkMHome
 
 :OkJHome
@@ -32,28 +33,31 @@ goto error
 
 :chkMHome
 set "MAVEN_PROJECTBASEDIR=%~dp0"
+
 @REM strip trailing backslash
 if "%MAVEN_PROJECTBASEDIR:~-1%"=="\" set "MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%"
 
-set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
-set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+set "WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
+set "WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain"
 
-set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.3.2/maven-wrapper-3.3.2.jar"
+set "DOWNLOAD_URL=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.3.2/maven-wrapper-3.3.2.jar"
 
 @REM First-run: download the wrapper JAR if missing.
-if not exist %WRAPPER_JAR% (
+if not exist "%WRAPPER_JAR%" (
     echo Downloading Maven Wrapper from %DOWNLOAD_URL% ...
-    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri %DOWNLOAD_URL% -OutFile %WRAPPER_JAR%}"
-    if not exist %WRAPPER_JAR% (
+    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile '%WRAPPER_JAR%'}"
+
+    if not exist "%WRAPPER_JAR%" (
         echo ERROR: failed to download Maven Wrapper.
         goto error
     )
 )
 
-%MAVEN_JAVA_EXE% ^
+@REM Launch Maven Wrapper
+"%MAVEN_JAVA_EXE%" ^
   %JVM_CONFIG_MAVEN_PROPS% ^
   %MAVEN_OPTS% ^
-  -classpath %WRAPPER_JAR% ^
+  -classpath "%WRAPPER_JAR%" ^
   "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" ^
   %WRAPPER_LAUNCHER% %*
 
@@ -66,3 +70,4 @@ set ERROR_CODE=1
 :end
 endlocal & set ERROR_CODE=%ERROR_CODE%
 exit /B %ERROR_CODE%
+```
